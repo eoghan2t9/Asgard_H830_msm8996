162c0a58f00367e38e9697196440c7b1681a23ff
Stephan Mueller <smueller@chronox.de>
2016-02-09 15:37:47 +0100

    crypto: xts - consolidate sanity check for keys
    The patch centralizes the XTS key check logic into the service function
    xts_check_key which is invoked from the different XTS implementations.
    With this, the XTS implementations in ARM, ARM64, PPC and S390 have now
    a sanity check for the XTS keys similar to the other arches.
    
    In addition, this service function received a check to ensure that the
    key != the tweak key which is mandated by FIPS 140-2 IG A.9. As the
    check is not present in the standards defining XTS, it is only enforced
    in FIPS mode of the kernel.
    
    Signed-off-by: Stephan Mueller <smueller@chronox.de>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit b879fc0a3e2eaad8224ecbb3c89598d84e08f5b7)
    
    Change-Id: Id1a9114e68e121f8bf97f7b30b525571f8256af3

--- arch/arm64/crypto/aes-glue.c
+++ arch/arm64/crypto/aes-glue.c
@@ -85,7 +85,15 @@ static int xts_set_key(struct crypto_tfm *tfm, const u8 *in_key,
 	struct crypto_aes_xts_ctx *ctx = crypto_tfm_ctx(tfm);
 	int ret;
 
+<<<<<<< HEAD
 	ret = aes_expandkey(&ctx->key1, in_key, key_len / 2);
+=======
+	ret = xts_check_key(tfm, in_key, key_len);
+	if (ret)
+		return ret;
+
+	ret = crypto_aes_expand_key(&ctx->key1, in_key, key_len / 2);
+>>>>>>> fd9fb49622e4... crypto: xts - consolidate sanity check for keys
 	if (!ret)
 		ret = aes_expandkey(&ctx->key2, &in_key[key_len / 2],
 				    key_len / 2);
