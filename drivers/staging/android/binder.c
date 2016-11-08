697ee1118dbcc337bd660f928ec99ded523f291b
Martijn Coenen <maco@android.com>
2016-11-08 20:12:16 +0100

    Android: binder: check set_context_mgr permission on time.
    Bug: 32394425
    Change-Id: I860c6aab97850bff05a56e96cd3f4b41691bfd96
    Signed-off-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2807,6 +2807,9 @@ static int binder_ioctl_set_ctx_mgr(struct file *filp)
 		ret = -EBUSY;
 		goto out;
 	}
+	ret = security_binder_set_context_mgr(proc->tsk);
+	if (ret < 0)
+		goto out;
 	if (uid_valid(binder_context_mgr_uid)) {
 		if (!uid_eq(binder_context_mgr_uid, curr_euid)) {
 			pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
@@ -2871,9 +2874,6 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	case BINDER_SET_CONTEXT_MGR:
 		ret = binder_ioctl_set_ctx_mgr(filp);
 		if (ret)
-			goto err;
-		ret = security_binder_set_context_mgr(proc->tsk);
-		if (ret < 0)
 			goto err;
 		break;
 	case BINDER_THREAD_EXIT:
