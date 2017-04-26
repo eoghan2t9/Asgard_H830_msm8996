fd4e7c28fb6f6e66f451096ad39983631f978985
lordodin912 <seanfrank912@gmail.com>
2017-04-26 17:38:13 -0400

    Fast charge

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -99,6 +99,11 @@ static int firstboot_check = 1;
 /* AHB2PHY read/write waite value */
 #define ONE_READ_WRITE_WAIT 0x11
 
+/* force high current otg */
+static bool highcurrent_otg = 0;
+module_param(highcurrent_otg, bool, S_IRUGO|S_IWUSR);
+MODULE_PARM_DESC(highcurrent_otg, "force high current charge with otg accessory");
+
 /* cpu to fix usb interrupt */
 static int cpu_to_affin;
 module_param(cpu_to_affin, int, S_IRUGO|S_IWUSR);
@@ -4503,15 +4508,35 @@ skip_psy_type:
 	/* Override mA if type-c charger used (use hvdcp/bc1.2 if it is 500) */
 	if (mdwc->typec_current_max > 500 && mA < mdwc->typec_current_max)
 		mA = mdwc->typec_current_max;
+	
 
 #if defined (CONFIG_LGE_TOUCH_CORE)
 	touch_notify_connect(mdwc->chg_type);
 #endif
 	if (mdwc->max_power == mA)
 		return 0;
-
+	
+	//Set high current otg current limit 
+	if (highcurrent_otg)
+		mA = DWC3_IDEV_CHG_MAX;
+	
 	dev_info(mdwc->dev, "Avail curr from USB = %u\n", mA);
-
+	
+	//Force high current otg charging for type-c docks
+	if (highcurrent_otg) {
+				
+		/* Enable Charging */
+		if (power_supply_set_online(&mdwc->usb_psy, true))
+			goto psy_error;
+		if (power_supply_set_current_limit(&mdwc->usb_psy, 1000*mA))
+			goto psy_error;
+		
+		power_supply_changed(&mdwc->usb_psy);
+	        mdwc->max_power = mA;
+		
+	      return 0;
+	}	
+	
 	if (mdwc->max_power <= 2 && mA > 2) {
 		/* Enable Charging */
 		if (power_supply_set_online(&mdwc->usb_psy, true))
