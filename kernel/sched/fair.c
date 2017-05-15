8e384946d890c07aa6de53ea725344035c697ae3
lordodin912 <seanfrank912@gmail.com>
2017-05-14 21:47:30 -0400

    Revert "Add Arch Power"
    This reverts commit 19eeae93960b307962f1116fd9edbc9e9bb27d39.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4710,16 +4710,10 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 }
 
 static unsigned int Lgentle_fair_sleepers = 0;
-static unsigned int Larch_power = 0;
 
 void relay_gfs(unsigned int gfs)
 {
 	Lgentle_fair_sleepers = gfs;
-}
-
-void relay_ap(unsigned int ap)
-{
-	Larch_power = ap;
 }
 
 static void
@@ -7920,7 +7914,6 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long capacity = SCHED_CAPACITY_SCALE;
 	struct sched_group *sdg = sd->groups;
 
-
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_cpu_capacity(sd, cpu);
 	else
@@ -7929,7 +7922,6 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
 	sdg->sgc->capacity_orig = capacity;
-
 
 	if (sched_feat(ARCH_CAPACITY))
 		capacity *= arch_scale_freq_capacity(sd, cpu);
