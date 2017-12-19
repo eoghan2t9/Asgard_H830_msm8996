6158022bd74e88537970750727ee15fa2b1fba98
eoghan2t9 <eoghan61@gmail.com>
2017-12-19 19:49:43 +0000

    read_write: some improvments

--- fs/read_write.c
+++ fs/read_write.c
@@ -382,7 +382,7 @@ ssize_t do_sync_read(struct file *filp, char __user *buf, size_t len, loff_t *pp
 	kiocb.ki_pos = *ppos;
 	kiocb.ki_nbytes = len;
 
-	ret = filp->f_op->aio_read(&kiocb, &iov, 1, kiocb.ki_pos);
+	ret = filp->f_op->aio_write(&kiocb, &iov, 1, *ppos);
 	if (-EIOCBQUEUED == ret)
 		ret = wait_on_sync_kiocb(&kiocb);
 	*ppos = kiocb.ki_pos;
